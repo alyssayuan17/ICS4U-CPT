517cbd05957513c53fc18c9bd9e05f18170a7fce
Alyssa Yuan <yutongalyssayuan@gmail.com>
2025-05-28 13:52:35 -0400

    test 2 working tree

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hiiiii");
+
+        hiMinna();
     }
 
+    public static void hiMinna(){
+        System.out.println("hi MIN-ah");
+    }
 }
