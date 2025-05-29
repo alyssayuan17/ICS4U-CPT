b06fb8cc009dcef8147fd32cca0ed87d98b62f4d
minnachoi123 <minnachoi0413@gmail.com>
2025-05-29 13:08:41 -0400

    so done bro

--- Main.java
+++ Main.java
@@ -1,12 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hiiiii");
-
-        hiMinna();
-    }
-
-    public static void hiMinna(){
-        System.out.println("hi MIN-ah");
+        System.out.println("im so done with ts");
     }
 }
