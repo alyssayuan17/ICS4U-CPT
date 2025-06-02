ad03170dd0a2921d9b65517630872a9e1dee6caa
Alyssa Yuan <yutongalyssayuan@gmail.com>
2025-06-02 12:34:10 -0400

    fixed static issue, created constructor for Account.java

--- Account.java
+++ Account.java
@@ -1,18 +1,30 @@
 import java.util.ArrayList;
 
 public class Account {
-    private static ArrayList <Day> days = new ArrayList<>(); //they can add days. each day has a breakfast, lunch, dinner 
-    private static ArrayList <Recipe> recipes = new ArrayList<>(); //contains preset 9 recipes + any custom recipes user creates 
+    private ArrayList <Day> days = new ArrayList<>(); //they can add days. each day has a breakfast, lunch, dinner 
+    private ArrayList <Recipe> recipes = new ArrayList<>(); //contains preset 9 recipes + any custom recipes user creates 
 
     //more instance variables that store preferences from initial dietary needs survey 
-    private static boolean vegetarian; 
-    private static String preferMeat; //options: no || red || seafood || poultry
-    private static boolean preferAltProtein; //beans, tofu, etc.
-    private static boolean preferCarbs;
-    private static String preferVeg; //options: green || red + orange
+    private boolean vegetarian; 
+    private String preferMeat; //options: no || red || seafood || poultry
+    private boolean preferAltProtein; //beans, tofu, etc.
+    private boolean preferCarbs;
+    private String preferVeg; //options: green || red + orange
     
-    public static Account accountSetupAndPreferenceSurvey(){
-        
+    // constructor
+    public Account(boolean vegetarian, String preferMeat, boolean preferAltProtein, boolean preferCarbs,
+            String preferVeg) {
+        this.vegetarian = vegetarian;
+        this.preferMeat = preferMeat;
+        this.preferAltProtein = preferAltProtein;
+        this.preferCarbs = preferCarbs;
+        this.preferVeg = preferVeg;
     }
+    
+    // public static Account accountSetupAndPreferenceSurvey(){
+        
+    // }
+
+    
 
 }
