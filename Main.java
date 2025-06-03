aaf58c92767765113ab119a0bbb6f5cb40266b6e
Alyssa Yuan <yutongalyssayuan@gmail.com>
2025-06-03 12:56:15 -0400

    fixed object issue with Ingredient and ArrayList add.

--- Main.java
+++ Main.java
@@ -4,30 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //preset recipes 
-        //recipe #1 - pasta 
-        String name = "pasta";
-        ArrayList <Ingredient> ingredients = new ArrayList <>();
-        ingredients.add();
-        Recipe pasta = 
-
         Account myAccount = Account.accountSetupAndPreferenceSurvey();
 
         ArrayList <Ingredient> vegetarianPizza = new ArrayList<>(); // veg, carbs
-        vegetarianPizza.add("Pizza dough", "250 g");
-        vegetarianPizza.add("Tomato sauce", "1/3 cup");
-        vegetarianPizza.add("Mozzarella cheese", "1/2 cup");
-        vegetarianPizza.add("Mushrooms", "3/4 cup");
-        vegetarianPizza.add("Black olives", "1/4 cup");
-        vegetarianPizza.add("Olive oil", "1 tbsp");
+        vegetarianPizza.add(new Ingredient("Pizza dough", "250 g"));
+        vegetarianPizza.add(new Ingredient("Tomato sauce", "1/3 cup"));
+        vegetarianPizza.add(new Ingredient("Mozzarella cheese", "1/2 cup"));
+        vegetarianPizza.add(new Ingredient("Mushrooms", "3/4 cup"));
+        vegetarianPizza.add(new Ingredient("Black olives", "1/4 cup"));
+        vegetarianPizza.add(new Ingredient("Olive oil", "1 tbsp"));
+        
         ArrayList <Ingredient> riceAndBeans = new ArrayList<>(); // veg, low carbs, beans
-        riceAndBeans.add("rice", "210 g");
+        riceAndBeans.add(new Ingredient("rice", "210 g"));
+        
         ArrayList <Ingredient> gardenSalad = new ArrayList<>(); // veg, low carbs, no beans, greens
+        
         ArrayList <Ingredient> moroccanBeetSalad = new ArrayList<>(); // veg, low carbs, no beans, red + orange
+        
         ArrayList <Ingredient> spaghettiBolognese = new ArrayList<>(); // not veg, no meat pref, no veg pref, carbs
+        
         ArrayList <Ingredient> eggCasserole = new ArrayList<>(); // not veg, no meat pref, no veg pref, low carbs
+        
         ArrayList <Ingredient> steak = new ArrayList<>(); // not veg, pref meat, red meat
+        
         ArrayList <Ingredient> smokedSalmon = new ArrayList<>(); // not veg, pref meat, seafood 
+        
         ArrayList <Ingredient> bakedChickenBreast = new ArrayList<>(); // not veg, pref meat, poultry
 
     }
