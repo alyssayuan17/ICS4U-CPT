8d2de4bef721553f7098eadf71edf51d76dd9d91
Alyssa Yuan <yutongalyssayuan@gmail.com>
2025-06-04 08:51:20 -0400

    added contructor, getters, and setters to Recipe.java

--- Recipe.java
+++ Recipe.java
@@ -3,6 +3,31 @@ import java.util.ArrayList;
 public class Recipe {
     //instance variables
     private String name; 
-    private ArrayList <Ingredient> ingredients = new ArrayList<>(); 
+    private ArrayList <Ingredient> ingredients = new ArrayList<>();
+    
+    public Recipe(String name, ArrayList<Ingredient> ingredients) {
+        this.name = name;
+        this.ingredients = ingredients;
+    }
+
+    // getters
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    // setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setIngredients(ArrayList<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    } 
 
 }
